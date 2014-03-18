26c157964706671915eb5fd457668ecceb9aec6a
MobyUniver <one2014@yandex.ru>
2014-03-18 23:10:43 +0200

    play video

--- src/com/gh/MobyUniver/ArticleFragment.java
+++ src/com/gh/MobyUniver/ArticleFragment.java
@@ -2,20 +2,23 @@ package com.gh.MobyUniver;
 
 import android.app.Fragment;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ArticleFragment extends Fragment {
     static View view;
-    static String id = "k";
+    String id;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.article_layout_fragment, container, false);
@@ -28,7 +31,13 @@ public class ArticleFragment extends Fragment {
 
         id = getActivity().getIntent().getExtras().getString("id");
         ((ImageView) view.findViewById(R.id.imageView)).setImageBitmap((Bitmap) getActivity().getIntent().getParcelableExtra("img"));
+        VideoView vid = (VideoView) view.findViewById(R.id.video);
         new JSONParseArticle().execute();
+        String videoSourse = "http://gkurs.esy.es/Command & Conquer 2013 (Generals 2) Alpha Gameplay Trailer.mp4";
+        vid.setVideoURI(Uri.parse(videoSourse));
+        vid.setMediaController(new MediaController(getActivity()));
+        vid.requestFocus(0);
+        vid.start();
     }
 
     private class JSONParseArticle extends AsyncTask<Void, Void, JSONArray> {
@@ -52,7 +61,6 @@ public class ArticleFragment extends Fragment {
             try {
                 JSONObject c = json.getJSONObject(0);
                ((TextView) view.findViewById(R.id.textView)).setText(c.getString("content"));
-
 
             } catch (JSONException e) {
                 e.printStackTrace();
